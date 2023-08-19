8a41f6185431a687715cce02196c89190eadc213
Fellipe Bravo <fellipe.bravo@gmail.com>
2023-08-19 09:01:18 -0300

    Main structure

---
+++ github-apis/github-apis/github_apisApp.swift
@@ -0,0 +1,17 @@
+//
+//  github_apisApp.swift
+//  github-apis
+//
+//  Created by 0xf on 19/08/23.
+//
+
+import SwiftUI
+
+@main
+struct github_apisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
